9fe997056e22cf6e867d029a3963c26b58150c0f
ShinDongJun <53344349+wq0212@users.noreply.github.com>
2021-02-15 03:18:37 +0900

    Delete test.py

--- test.py
+++
@@ -1,9 +0,0 @@
-import OpenDartReader
-api_key = 'd92e1e01ed0185352a4b4ca637c89b4f83acae78'
-
-dart = OpenDartReader(api_key)
-
-# 단일기업 전체 재무제표 (삼성전자 2018 전체 재무제표)
-dart.finstate_all('005930', 2018)
-
-print(dart.finstate_all('005930', 2018))
